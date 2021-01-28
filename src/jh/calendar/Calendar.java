b2e55a3c9a490d019da53d9b11aad1912a839012
dlw08 <dlw08@DESKTOP-U8EH0NA>
2021-01-28 17:06:33 +0900

    Use method

--- src/jh/calendar/Calendar.java
+++ src/jh/calendar/Calendar.java
@@ -3,6 +3,11 @@ package jh.calendar;
 import java.util.Scanner;
 
 public class Calendar {
+	public int getDaysOfMonth(int month) { 
+		int[] daysOfMonth = new int[] {31,28,31,30,31,30,31,31,30,31,30,31};  
+		return daysOfMonth[month -1];
+	}
+	
 	public static void main(String[] args) {
 		System.out.println(" 일 월 화 수 목 금 토");
 		System.out.println("---------------------");
@@ -14,11 +19,10 @@ public class Calendar {
 		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
 		System.out.println("달을 입력하세요.");
 		Scanner scan = new Scanner(System.in);
+		Calendar cal = new Calendar();
 		int month = scan.nextInt();
-		
-		int[] monthD = new int[] {31,28,31,30,31,30,31,31,30,31,30,31};
-		
-		System.out.println(month + "월은 " + monthD[month-1] + "일까지 있습니다.");
+			
+		System.out.println(month + "월은 " + cal.getDaysOfMonth(month)  + "일까지 있습니다.");
 		scan.close();		
 	}
 }
